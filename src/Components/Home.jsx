97e02b4ec1fb5cc457b648d62582e552d44f4ef9
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-03-23 10:17:02 +0700

    make logic and styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,16 +3,36 @@ import data from "./data";
 
 function Home() {
   const api_url = data.album;
-
   console.log(api_url.images[0]);
+  console.log(api_url.artists[0].name);
+
+  // var btnShow = document.getElementById("btnDisplay");
+
+  // btnShow.onclick = function () {
+  //   var ds = document.getElementById("Show");
+  //   if (ds.style.display !== "none") {
+  //     ds.style.display = "none";
+  //   } else {
+  //     ds.style.display = "block";
+  //   }
+  // };
 
   return (
     <div className="callDataPage ">
-      <div className="w-100 d-flex justify-content-center mt-5 mb-5">
-        <div className="row card col-11 mainCall">
+      {/* <div className="w-100 d-flex justify-content-center text-center mt-5">
+        <button
+          className="btn btn-lg btn-block text-center btn-success"
+          id="btnDisplay"
+        >
+          Get Show
+        </button>
+      </div> */}
+
+      <div className="w-100 d-flex justify-content-center mt-5 mb-5 ">
+        <div className="row card col-11 mainCall Show " id="Show">
           <div className="col mt-5 mb-3">
             <div className="container ">
-              <div className="w-100 d-flex justify-content-center gam ">
+              <div className="w-100 d-flex justify-content-center  ">
                 <img
                   className=" rounded-circle gambar"
                   src={api_url.images[0].url}
@@ -28,11 +48,18 @@ function Home() {
               <h3>
                 <b>{api_url.name}</b>
               </h3>
-              <h5>{api_url.release_date}</h5>
-              <h5>{api_url.release_date_precision}</h5>
-              <h5>{api_url.total_tracks}</h5>
-              <h5>{api_url.type}</h5>
-              <h5>{api_url.uri}</h5>
+              <h5>
+                <b>{api_url.artists[0].name}</b>
+              </h5>
+              <h6>{api_url.release_date}</h6>
+              <h6>
+                {api_url.type} {api_url.total_tracks}
+              </h6>
+              <a href={api_url.uri}>
+                <button className="btn btn-success    mt-3 shadow p-2 col-12 col-sm-2 ">
+                  Select
+                </button>
+              </a>
             </div>
           </div>
         </div>
