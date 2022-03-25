b85714fd2db8cfc7b32f1af5d2cb960cc8d5155e
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-03-25 11:31:06 +0700

    add export component to deferent component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import data from "./data";
+
+import { MyComponentFunc } from "./Main/MyComponents";
 
 function Home() {
-  const api_url = data.album;
-  console.log(api_url.images[0]);
-  console.log(api_url.artists[0].name);
-
   // var btnShow = document.getElementById("btnDisplay");
 
   // btnShow.onclick = function () {
@@ -19,6 +16,7 @@ function Home() {
 
   return (
     <div className="callDataPage ">
+      <MyComponentFunc />
       {/* <div className="w-100 d-flex justify-content-center text-center mt-5">
         <button
           className="btn btn-lg btn-block text-center btn-success"
@@ -27,43 +25,6 @@ function Home() {
           Get Show
         </button>
       </div> */}
-
-      <div className="w-100 d-flex justify-content-center mt-5 mb-5 ">
-        <div className="row card col-11 mainCall Show " id="Show">
-          <div className="col mt-5 mb-3">
-            <div className="container ">
-              <div className="w-100 d-flex justify-content-center  ">
-                <img
-                  className=" rounded-circle gambar"
-                  src={api_url.images[0].url}
-                  id="imge"
-                  alt=""
-                  width="30%"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="col  mb-5">
-            <div className="container text-center">
-              <h3>
-                <b>{api_url.name}</b>
-              </h3>
-              <h5>
-                <b>{api_url.artists[0].name}</b>
-              </h5>
-              <h6>{api_url.release_date}</h6>
-              <h6>
-                {api_url.type} {api_url.total_tracks}
-              </h6>
-              <a href={api_url.uri}>
-                <button className="btn btn-success    mt-3 shadow p-2 col-12 col-sm-2 ">
-                  Select
-                </button>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
